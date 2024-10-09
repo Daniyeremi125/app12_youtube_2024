42af2c65e31e54e2be3cce531c849e5fe1c0e578
Daniyeremi12345 <202100352G@utea.edu.pe>
2024-10-09 00:11:10 -0500

    usando la clave unica

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,12 +1,36 @@
+import 'package:app12_youtube_2024/models/video_model.dart';
+import 'package:app12_youtube_2024/services/api_service.dart';
 import 'package:app12_youtube_2024/ui/general/colors.dart';
 import 'package:app12_youtube_2024/ui/widgets/item_filter_widget.dart';
+import 'package:app12_youtube_2024/ui/widgets/item_video_widget.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final APIService _apiService = APIService();
+
+  List<VideoModel> videos = [];
+
+  @override
+  void initState() {
+    super.initState();
+    getData();
+  }
+
+  getData() {
+    _apiService.getVideos().then((value) {
+      videos = value;
+      setState(() {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
+    _apiService.getVideos();
     return SingleChildScrollView(
       child: Padding(
         padding: EdgeInsets.symmetric(
@@ -65,7 +89,16 @@ class HomePage extends StatelessWidget {
                   ),
                 ],
               ),
-            )
+            ),
+            ListView.builder(
+                shrinkWrap: true,
+                physics: ScrollPhysics(),
+                itemCount: videos.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ItemVideoWidget(
+                    videoModel: videos[index],
+                  );
+                }),
           ],
         ),
       ),
---
+++ lib/utils/constants.dart
@@ -0,0 +1,2 @@
+String pathProduction = "https://youtube.googleapis.com/youtube/v3/";
+String apiKey = "AIzaSyDFM46dXSLM5yxkM2CkRhygUbfvhHehuMY"; //Clave unica
